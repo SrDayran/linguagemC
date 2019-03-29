69a6c8c66e62f268231aecaa6b298e32ff9c631e
brunosana <brunosana@outlook.com.br>
2019-03-29 20:43:30 -0300

    criada classe para criptografia de teste em string

--- Estudos1/src/TesteCriptografia/arquivo.java
+++ Estudos1/src/TesteCriptografia/arquivo.java
@@ -11,6 +11,14 @@ import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.security.InvalidKeyException;
+import java.security.NoSuchAlgorithmException;
+import javax.crypto.BadPaddingException;
+import javax.crypto.Cipher;
+import javax.crypto.IllegalBlockSizeException;
+import javax.crypto.KeyGenerator;
+import javax.crypto.NoSuchPaddingException;
+import javax.crypto.SecretKey;
 import javax.swing.JFileChooser;
 import javax.swing.JOptionPane;
 import javax.swing.filechooser.FileNameExtensionFilter;
@@ -34,5 +42,37 @@ public class arquivo {
                 JOptionPane.showMessageDialog(null, ex.getMessage());
             }
     
-}
+    }
+    
+    public void criptografar () throws NoSuchAlgorithmException, NoSuchPaddingException, InvalidKeyException, IllegalBlockSizeException, BadPaddingException {
+     KeyGenerator keygenerator = KeyGenerator.getInstance("DES");
+     SecretKey chaveDES = keygenerator.generateKey();
+ 
+     Cipher cifraDES;
+ 
+     // Cria a cifra 
+     cifraDES = Cipher.getInstance("DES/ECB/PKCS5Padding");
+ 
+     // Inicializa a cifra para o processo de encriptação
+     cifraDES.init(Cipher.ENCRYPT_MODE, chaveDES);
+ 
+     // Texto puro
+     byte[] textoPuro = "Exemplo de texto puro".getBytes();
+ 
+     System.out.println("Texto [Formato de Byte] : " + textoPuro);
+     System.out.println("Texto Puro : " + new String(textoPuro));
+ 
+     // Texto encriptado
+     byte[] textoEncriptado = cifraDES.doFinal(textoPuro);
+ 
+     System.out.println("Texto Encriptado : " + textoEncriptado);
+ 
+     // Inicializa a cifra também para o processo de decriptação
+     cifraDES.init(Cipher.DECRYPT_MODE, chaveDES);
+ 
+     // Decriptografa o texto
+     byte[] textoDecriptografado = cifraDES.doFinal(textoEncriptado);
+ 
+     System.out.println("Texto Decriptografado : " + new String(textoDecriptografado)); 
+    }
 }
